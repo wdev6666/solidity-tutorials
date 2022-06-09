30cefa2ed7175077ec0fdf42123607df00b31781
Naresh <wdev6666@gmail.com>
2022-06-09 09:28:04 +0530

    ICO Frontend

--- initial-coin-offering/front-end/pages/index.js
+++ initial-coin-offering/front-end/pages/index.js
@@ -1,31 +1,286 @@
-import { useState } from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useRef, useState } from "react";
+import { BigNumber, Contract, providers, utils } from "ethers";
+import Web3Modal from "web3modal";
+import Head from "next/head";
+import {
+  NFT_CONTRACT_ADDRESS,
+  NFT_CONTRACT_ABI,
+  TOKEN_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+} from "../constants";
+
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-
+  const zero = BigNumber.from(0);
   const [walletConnected, setWalletConnected] = useState(false);
   const [balanceOfNFTTokens, setBalanceOfNFTTokens] = useState(0);
-  const [tokensMinted, setTokenMinted] = useState(0);
+  const [tokensMinted, setTokensMinted] = useState(0);
   const [loading, setLoading] = useState(false);
   const [isOwner, setIsOwner] = useState(false);
-  const [tokenToBeClaimed, setTokenToBeClaimed] = useState(0);
-  const connectWallet = () => { };
+  const [tokensToBeClaimed, setTokensToBeClaimed] = useState(0);
+  const [tokenAmount, setTokenAmount] = useState(0);
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+    if (needSigner) {
+      const singer = web3Provider.getSigner();
+      return singer;
+    }
+
+    return web3Provider;
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getOwner = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
+      const _owner = await nftContract.owner();
+      const signer = await getProviderOrSigner(true);
+      const address = await signer.getAddress();
+      if (address.toLowerCase() === _owner.toLowerCase()) {
+        setIsOwner(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const withdrawCoins = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+      const transaction = await nftContract.withdraw();
+      setLoading(true);
+      await transaction.wait();
+      setLoading(false);
+      await getOwner();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const claimNFTTokens = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+      const tx = await tokenContract.claim();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      window.alert("Sucessfully claimed NFT Tokens");
+      await getBalanceOfCryptoDevTokens();
+      await getTotalTokensMinted();
+      await getTokensToBeClaimed();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const mintNFTToken = async (amount) => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+      const value = 0.001 * amount;
+      const transaction = await nftContract.mint(amount, {
+        value: utils.parseEther(value.toString()),
+      });
+      setLoading(true);
+      await transaction.wait();
+      setLoading(false);
+      window.alert("Sucessfully minted NFT Tokens");
+      await getBalanceOfNFTTokens();
+      await getTotalTokensMinted();
+      await getTokensToBeClaimed();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getTotalTokensMinted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
+      const signer = await getProviderOrSigner(true);
+      const address = await signer.getAddress();
+      const balance = await nftContract.balanceOf(address);
+      if (balance === zero) {
+        setTokensToBeClaimed(zero);
+      } else {
+        var amount = 0;
+        for (var i = 0; i < balance; i++) {
+          const tokenId = await nftContract.tokenOfOwnerByIndex(address, i);
+          const claimed = await tokenContract.tokenIdsClaimed(tokenId);
+          if (!claimed) {
+            amount++;
+          }
+        }
+        setTokensToBeClaimed(BigNumber.from(amount));
+      }
+    } catch (err) {
+      console.error(err);
+      setTokensToBeClaimed(zero);
+    }
+  };
+
+  const getBalanceOfNFTTokens = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
+      const signer = await getProviderOrSigner(true);
+      const address = await signer.getAddress();
+      const balance = await nftContract.balanceOf(address);
+      setBalanceOfNFTTokens(balance);
+    } catch (err) {
+      console.error(err);
+      setBalanceOfNFTTokens(zero);
+    }
+  };
+
+  const getTokensToBeClaimed = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
+      const _tokensMinted = await nftContract.totalSupply();
+      setTokensMinted(_tokensMinted);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const renderButton = () => {
-    return;
-  }
+    if (loading) {
+      return (
+        <div>
+          <button className={styles.button}>Loading...</button>
+        </div>
+      );
+    }
+
+    if (walletConnected && isOwner) {
+      return (
+        <div>
+          <button className={styles.button1} onClick={withdrawCoins}>
+            Withdraw Coins
+          </button>
+        </div>
+      );
+    }
+
+    // If token to be claimed are greater than 0
+    if (tokensToBeClaimed > 0) {
+      return (
+        <div>
+          <div className={styles.description}>
+            {tokensToBeClaimed * 10} Tokens can be claimed!
+          </div>
+          <button className={styles.button} onClick={claimNFTTokens}>
+            Claim Tokens
+          </button>
+        </div>
+      );
+    }
+
+    // If user does not have any tokens to claim
+    return (
+      <div style={{ display: "flex-col" }}>
+        <div>
+          <input
+            type="number"
+            placeholder="Amount of Tokens"
+            // BigNumber.from converts the `e.target.value` to a BigNumber
+            onChange={(e) => setTokenAmount(BigNumber.from(e.target.value))}
+            className={styles.input}
+          />
+        </div>
+
+        <button
+          className={styles.button}
+          disabled={!(tokenAmount > 0)}
+          onClick={() => mintNFTToken(tokenAmount)}
+        >
+          Mint Tokens
+        </button>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+      getTotalTokensMinted();
+      getBalanceOfNFTTokens();
+      getTokensToBeClaimed();
+      withdrawCoins();
+    }
+  }, [walletConnected]);
 
   return (
     <div>
       <Head>
-        <title>Crypto Devs</title>
+        <title>NFT Tokens</title>
         <meta name="description" content="ICO-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>Welcome to Blockchain Developers ICO!</h1>
+          <h1 className={styles.title}>
+            Welcome to Blockchain Developers ICO!
+          </h1>
           <div className={styles.description}>
             You can claim or mint NFT tokens here
           </div>
@@ -33,11 +288,13 @@ export default function Home() {
             <div>
               <div className={styles.description}>
                 {/* Format Ether helps us in converting a BigNumber to string */}
-                You have minted {utils.formatEther(balanceOfNFTTokens)} NFT Tokens
+                You have minted {utils.formatEther(balanceOfNFTTokens)} NFT
+                Tokens
               </div>
               <div className={styles.description}>
                 {/* Format Ether helps us in converting a BigNumber to string */}
-                Overall {utils.formatEther(tokensMinted)}/10000 have been minted!!!
+                Overall {utils.formatEther(tokensMinted)}/10000 have been
+                minted!!!
               </div>
               {renderButton()}
             </div>
@@ -56,5 +313,5 @@ export default function Home() {
         Made with &#10084; by &nbsp; <b>Naresh</b>
       </footer>
     </div>
-  )
+  );
 }
