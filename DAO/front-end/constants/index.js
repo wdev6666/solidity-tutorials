e49a46c44e5bf9f259a1ef23f5d79549ed94727a
Naresh <wdev6666@gmail.com>
2022-06-10 09:16:43 +0530

    DAO Front-end Added

---
+++ DAO/front-end/constants/index.js
@@ -0,0 +1,6 @@
+export const DAO_CONTRACT_ADDRESS = "";
+export const NFT_CONTRACT_ADDRESS =
+  "0x92C27c69Dec4E97D0c354547B679F48F49fA56F4";
+
+export const DAO_ABI = [];
+export const NFT_ABI = [];
